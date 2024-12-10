3bd16f69875de469004d485f03c2e1cef5f4c54c
Gurpreet Chana <gurpreetchana_@outlook.com>
2024-12-10 15:32:57 +0000

    change port to pull from env var

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ import tauntRoutes from "./routes/tauntRoutes.js";
 dotenv.config();
 
 const app = express();
-const port = 5050;
-const { API_KEY } = process.env;
-const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const port = process.env.PORT;
 
 const { ORIGIN } = process.env;
 app.use(cors());
+const { API_KEY } = process.env;
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 app.use("/revenge", revengeRoutes);
 app.use("/taunt", tauntRoutes);
